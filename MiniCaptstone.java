dba6718a40ec97e90ac37759debcb78912608a79
Tgschwendtner <blues87@live.com>
2016-05-11 11:05:17 -0500

    5/10/16

--- MiniCaptstone.java
+++ MiniCaptstone.java
@@ -1,49 +1,66 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.GridLayout;
-//https://docs.oracle.com/javase/7/docs/api/java/awt/GridLayout.html#GridLayout(int,%20int)
+import java.awt.GridBagLayout;
+
 public class MiniCaptstone extends JFrame
 {
     private static final int FRAME_WIDTH = 500;
     private static final int FRAME_HEIGHT = 500;
-    private int hgap = 10;
-    private int vgap = 10;
-    private int rows = 3;
-    private int cols = 3;
+    
     
     private JFrame frame;
     private JPanel panel;
+    
     private JTextField totalMinutes;
     private JTextField totalDistance;
     private JTextField totalSeconds;
     private JTextField totalHours;
     private JButton calculate;
+    private JLabel minuteLabel;
+    private JLabel secondLabel;
+    private JLabel hourLabel;
+    private JLabel distanceLabel;
+    
     private String[] description = { "Miles", "Kilometers" };
     private JComboBox <String> distanceSystem;
     public MiniCaptstone()
     {
         this.frame = new JFrame();
         this.panel = new JPanel();
-
+       
+        
+        
         this.totalMinutes = new JTextField(15);
         this.totalSeconds = new JTextField(15);
         this.totalDistance = new JTextField(15);
         this.totalHours = new JTextField(15);
         this.calculate = new JButton ( "Calculate Split" );
         this.distanceSystem = new JComboBox <String>();
-        this.panel.add( this.calculate );
+        
+        calculate.setLocation(100,50);
+        this.panel.add(calculate);
+        
+        totalMinutes.setLocation(100,200);
         this.panel.add(totalMinutes);
+        
+        totalSeconds.setLocation(50,300);
         this.panel.add(totalSeconds);
+        
+        totalDistance.setLocation(90,40);
         this.panel.add(totalDistance);
+        
+        totalHours.setLocation(0,0);
         this.panel.add(totalHours);
         
-        GridLayout layout = new GridLayout( rows, cols, hgap, vgap);
         
+        
+      
         
         for (int i = 0; i < description.length;i++)
         {
@@ -70,26 +87,25 @@ public class MiniCaptstone extends JFrame
 
         public void actionPerformed( ActionEvent event )
         {
-            String minutes = totalMinutes.getText();
-            String seconds = totalSeconds.getText();
-            String hours = totalHours.getText();
-            String distance = totalDistance.getText();
+            double minutes = Double.parseDouble(totalMinutes.getText());
+            double seconds = Double.parseDouble(totalSeconds.getText());
+            double hours = Double.parseDouble(totalHours.getText());
+            double distance = Double.parseDouble(totalDistance.getText());
             //math time (minutes) * 60 + seconds / distance / 60 = minutes
             //math time (minutes) * 60 + seconds / distance - minutes = seconds
             if ( distanceSystem.getSelectedIndex() == 0 )
             {
                 //Math goes here
-                System.out.println("Minutes "+ minutes);
-                System.out.println("Seconds "+ seconds);
-                System.out.println("Hours "+ hours);
-                System.out.println("Distance "+ distance);
-                System.out.println("Miles");
+                int mileSplitMinutes = (int)(((hours*60*60 + minutes*60 + seconds)/distance) /60);
+                int mileSplitSeconds = (int) (((hours*60*60 + minutes*60 + seconds)/distance) - (mileSplitMinutes*60));
+                System.out.println( mileSplitMinutes + ":" + mileSplitSeconds);
             }
             else if (distanceSystem.getSelectedIndex() == 1 )
             {
                 //Math goes here
-                
-                System.out.println("Kilometers");
+                int KiloSplitMinutes = (int)(((hours*60*60 + minutes*60 + seconds)/distance) /60);
+                int KiloSplitSeconds = (int) (((hours*60*60 + minutes*60 + seconds)/distance) - (KiloSplitMinutes*60));
+                System.out.println( KiloSplitMinutes + ":" + KiloSplitSeconds);
             }
             
         }
